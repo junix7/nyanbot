895c2ad17f489febc29b23d1ab95ad5ba3e8dbbf
[junix7] <juliop.unix@gmail.com>
2020-10-12 13:03:44 -0500

    env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+require('dotenv').config();
+const cooldown = new Set()
 /*client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });*/
